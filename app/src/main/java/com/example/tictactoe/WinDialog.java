4719fda4da2b142097f0981d3febd84558afa608
khlifnihel <khlifnihel@fsm.u-monastir.tn>
2023-12-20 13:11:35 +0100

    first commit

---
+++ app/src/main/java/com/example/tictactoe/WinDialog.java
@@ -0,0 +1,39 @@
+package com.example.tictactoe;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class WinDialog extends Dialog {
+    private final String message;
+    private final MainActivity mainActivity;
+
+    public WinDialog(Context context, String message, MainActivity mainActivity) {
+        super(context);
+        this.message = message;
+        this.mainActivity = mainActivity;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.win_dialog_layout);
+
+        final TextView messageTxt = findViewById(R.id.messageTxt);
+        final Button startAgainBtn = findViewById(R.id.satrtAgainBtn);
+
+        messageTxt.setText(message);
+
+        startAgainBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mainActivity.restartMatch();
+                dismiss();
+            }
+        });
+    }
+}
